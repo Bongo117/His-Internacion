0c6754e2160a9c3df6707e8fbd2c3cc3e44780db
Bongo117 <luchosoto117@gmail.com>
2025-05-19 21:30:20 -0300

    me comí la o

--- Rutas/Admisiones.js
+++ Rutas/Admisiones.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const db      = require('../models/db');  
+const db      = require('../modelos/db');  
 
 
 router.get('/admitir', (req, res) => {
