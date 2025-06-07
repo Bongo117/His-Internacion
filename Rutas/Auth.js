96baf9941dd933726b5d67214e3eccadd27958b8
Bongo117 <luchosoto117@gmail.com>
2025-06-07 20:10:48 -0300

    creando login

---
+++ Rutas/Auth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/login', (req, res) => {
+  res.render('login', { titulo: 'Login' });
+});
+
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === 'admin' && password === '1234') {
+    req.session.user = username;
+    res.redirect('/'); 
+  } else {
+    res.render('login', { titulo: 'Login', error: '⚠️ Usuario o contraseña incorrectos' });
+  }
+});
+
+
+router.get('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Error al cerrar sesión:', err);
+      return res.send('Error al cerrar sesión.');
+    }
+    res.redirect('/login');
+  });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const app = express();
+
 const rutaAdmisiones = require('./Rutas/Admisiones');
 const rutaPacientes  = require('./Rutas/Pacientes');
 const rutaCamas = require('./Rutas/Camas');
+const authRoutes = require('./Rutas/auth');
 
-
-const app = express();
+app.use(session({
+  secret: 'super-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'vistas'));
+app.set('views', path.join(__dirname, 'Vistas'));
 
 
 app.use(express.static(path.join(__dirname, 'publico')));
-app.use(express.urlencoded({ extended: false })); 
+app.use(express.urlencoded({ extended: false }));
 
+app.use(authRoutes); 
+app.use('/', rutaAdmisiones);
+app.use('/', rutaPacientes);
+app.use('/', rutaCamas);
 
 app.get('/', (req, res) => {
   res.render('index', { titulo: 'Módulo de Admisión HIS' });
 });
-
-
-app.use('/', rutaAdmisiones);
-app.use('/', rutaPacientes);
-app.use('/', rutaCamas);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
